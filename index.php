5ca40ec39af8fcd9adfddd6b073fedeb7a97b62f
Moophie <michael.van.lierde@hotmail.com>
2020-03-20 15:44:38 +0100

    Index.php start

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <title>Home</title>
+</head>
+<body>
+    <h1>Welcome in IMD</h1>
+    <h2 class="text-muted">Example of muted bootstrap text</h2>
+</body>
+</html>
