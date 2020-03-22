8a7db29abfc93207e88df82c250d9a484d754d8f
Jasperator <55495886+Jasperator@users.noreply.github.com>
2020-03-22 14:48:33 +0100

    added good looking register page

---
+++ classes/User.php
@@ -0,0 +1,102 @@
+<?php 
+
+include_once(__DIR__ . "./Db.php");
+
+class User {
+
+    private $fullname;
+    private $email;
+    private $password;
+
+      /**
+     * Get the value of fullname
+     */ 
+    public function getFullname()
+    {
+        return $this->fullname;
+    }
+
+    /**
+     * Set the value of fullname
+     *
+     * @return  self
+     */ 
+    public function setFullname($fullname)
+    {
+        $this->fullname = $fullname;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of email
+     */ 
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set the value of email
+     *
+     * @return  self
+     */ 
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of password
+     */ 
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set the value of password
+     *
+     * @return  self
+     */ 
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+
+    public function save() {
+        //db conn
+        $conn = Db::getConnection();
+        //insert query
+        $statement = $conn->prepare("insert into users (fullname, email, password) values (:fullname, :email, :password)");
+        $firstname = $this->getFullname();
+        $lastname = $this->getEmail();
+        $email = $this->getPassword();
+        
+        $statement->bindValue(":fullname", $fullname);
+        $statement->bindValue(":email", $email);
+        $statement->bindValue(":password", $password);
+        //return result
+        $result = $statement->execute();
+
+        return $result;
+    }
+
+    public static function getAll() {
+        //db conn
+        $conn = Db::getConnection();
+
+        $statement = $conn->prepare("select * from users");
+        
+        $statement->execute();
+        $users = $statement->fetchAll(PDO::FETCH_ASSOC);
+        return $users;
+
+    }
+
+}
--- register.php
+++ register.php
@@ -1,12 +1,12 @@
 <?php 
-    include_once(__DIR__ . "/classes/Student.php");
+    include_once(__DIR__ . "/classes/User.php");
 
     if(!empty($_POST)) {
         try {
             $user = new User();
-            $user->setFirstname($_POST['firstname']);
-            $user->setLastname($_POST['lastname']);
-            $user->setEmail($_POST['email']);
+            $user->setFirstname($_POST['fullname']);
+            $user->setLastname($_POST['email']);
+            $user->setEmail($_POST['password']);
 
             $user->save();//active record patroon
             $success = "user saved";
@@ -20,42 +20,40 @@
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+<meta charset="utf-8">
+	<title>Register</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<!-- Font-->
+	<link rel="stylesheet" type="text/css" href="css/roboto-font.css">
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-5/css/fontawesome-all.min.css">
+	<!-- Main Style Css -->
+    <link rel="stylesheet" href="css/style_register.css"/>
 </head>
-<body>
-    <?php if(isset($error)) : ?>
-    <div class="error" style="color: red;"><?php echo $error; ?></div>
-    <?php endif; ?>
-
-    <?php if(isset($success)) : ?>
-    <div class="success" style="color: green;"><?php echo $success; ?></div>
-    <?php endif; ?>
-
-    <form action="" method="post">
-        <div>
-            <label for="firstname">firstname</label>
-            <input type="text" name="firstname" id="firstname">
-        </div>
-        
-        <div>
-            <label for="lastnamename">lastname</label>
-            <input type="text" name="lastname" id="lastname">
-        </div>
-
-        <div>
-            <label for="email">email</label>
-            <input type="text" name="email" id="email">
-        </div>
-
-        <div>
-            <input type="submit" value="Sign me up">
-        </div>
-    </form>
-
-    <?php foreach($users as $user): ?>
-        <h2><?php echo $user['email']; ?></h2>
-    <?php endforeach; ?>
+<body class="form-v5">
+	<div class="page-content">
+		<div class="form-v5-content">
+			<form class="form-detail" action="#" method="post">
+				<h2>Register Account Byddy application</h2>
+				<div class="form-row">
+					<label for="fullname">Full Name</label>
+					<input type="text" name="fullname" id="fullname" class="input-text" placeholder="Your Name" required>
+					<i class="fas fa-user"></i>
+				</div>
+				<div class="form-row">
+					<label for="email">Your Email</label>
+					<input type="text" name="email" id="email" class="input-text" placeholder="Your Email" required pattern="[^@]+@[^@]+.[a-zA-Z]{2,6}">
+					<i class="fas fa-envelope"></i>
+				</div>
+				<div class="form-row">
+					<label for="password">Password</label>
+					<input type="password" name="password" id="password" class="input-text" placeholder="Your Password" required>
+					<i class="fas fa-lock"></i>
+				</div>
+				<div class="form-row-last">
+					<input type="submit" name="register" class="register" value="Register">
+				</div>
+			</form>
+		</div>
+	</div>
 </body>
 </html>
