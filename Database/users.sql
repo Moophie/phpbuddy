327574b90eef8a58391dfa28328f241be229fcd8
Moophie <michael.van.lierde@hotmail.com>
2020-03-24 00:16:07 +0100

    Fixed database using wrong charset, added some example users

--- Database/users.sql
+++ Database/users.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 5.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Gegenereerd op: 23 mrt 2020 om 19:18
--- Serverversie: 10.1.38-MariaDB
--- PHP-versie: 7.2.28
+-- Gegenereerd op: 24 mrt 2020 om 00:14
+-- Serverversie: 10.4.11-MariaDB
+-- PHP-versie: 7.4.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -30,10 +30,19 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
-  `fullname` varchar(300) NOT NULL,
-  `email` varchar(300) NOT NULL,
-  `password` varchar(300) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `fullname` varchar(300) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(300) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(300) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `users`
+--
+
+INSERT INTO `users` (`id`, `fullname`, `email`, `password`) VALUES
+(1, 'Test McTesterson', 'test@student.thomasmore.be', '$2y$10$MRfrQW5t9w.zGuH806W6GeUoOrEWyZDTeBo/iNqMCtZ4ZbOn8XesC'),
+(2, 'Wombat Awesome', 'womb@student.thomasmore.be', '$2y$10$.qEfGQrIXYjb/md.1KkWoelaJ8NW8s5BSSpQ0D3eCnzr.6hs/yva6'),
+(3, 'Jan Jansen', 'janjansen@student.thomasmore.be', '$2y$10$MEsCuoquazX48.EXgJflGeZNGfShqNAA.7mzipwiyFIiyxV89QRZS');
 
 --
 -- Indexen voor geëxporteerde tabellen
@@ -53,7 +62,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT voor een tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=63;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
