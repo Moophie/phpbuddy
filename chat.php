fba8136955aa32e4862d11afdd4b05405e8d8803
Jasperator <55495886+Jasperator@users.noreply.github.com>
2020-04-15 13:25:19 +0200

    Start of reaction system on messages

--- chat.php
+++ chat.php
@@ -50,6 +50,15 @@ if (!empty($_POST['sendMessage'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="css/phpbuddy.css">
+    <!-- bootstrap css -->
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="css/style.css" />
+<!-- Css for reaction system -->
+<link rel="stylesheet" type="text/css" href="css/reaction.css" />
+ 
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<!-- jQuery for Reaction system -->
+<script type="text/javascript" src="js/reaction.js"></script>
     <title>Chat</title>
     <style>
         .chat {
@@ -76,6 +85,27 @@ if (!empty($_POST['sendMessage'])) {
             foreach ($messages as $message) : ?>
                 <p><strong><?= htmlspecialchars($message->fullname) ?></strong></p>
                 <p><?= htmlspecialchars($message->content) ?></p>
+<!-- Reaction system start -->
+<div class="reaction-container"><!-- container div for reaction system -->
+<span class="reaction-btn"> <!-- Default like button -->
+<span class="reaction-btn-emo like-btn-default"></span> <!-- Default like button emotion-->
+<span class="reaction-btn-text">Like</span> <!-- Default like button text,(Like, wow, sad..) default:Like  -->
+<ul class="emojies-box"> <!-- Reaction buttons container-->
+<li class="emoji emo-like" data-reaction="Like"></li>
+<li class="emoji emo-love" data-reaction="Love"></li>
+<li class="emoji emo-haha" data-reaction="HaHa"></li>
+<li class="emoji emo-wow" data-reaction="Wow"></li>
+<li class="emoji emo-sad" data-reaction="Sad"></li>
+<li class="emoji emo-angry" data-reaction="Angry"></li>
+</ul>
+</span>
+<div class="like-stat"> <!-- Like statistic container-->
+<span class="like-emo"> <!-- like emotions container -->
+<span class="like-btn-like"></span> <!-- given emotions like, wow, sad (default:Like) -->
+</span>
+</div>
+</div>
+<!-- Reaction system end -->
             <?php endforeach; ?>
         </div>
         <textarea name="content" id="" cols="30" rows="1"></textarea>
