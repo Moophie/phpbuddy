c344049705bae472d5e91da600f7970b0c04c57f
Jasperator <55495886+Jasperator@users.noreply.github.com>
2020-04-26 16:46:45 +0200

    Feature 15 works, code is not clean

--- emailverification.php
+++ emailverification.php
@@ -3,15 +3,21 @@ include_once(__DIR__ . "/classes/Db.php");
 include_once(__DIR__ . "/classes/User.php");
 
 
-$conn = Db::getConnection();
+
+$conn = mysqli_connect("localhost", "root", "", "phpbuddy"); 
+
 
 if(isset($_POST["email"]))
 {
+
  $email = mysqli_real_escape_string($conn, $_POST["email"]);
- $query = "SELECT * FROM users WHERE email = '".$email."'";
+
+ $query = "SELECT * FROM users WHERE email = '$email'";
  $result = mysqli_query($conn, $query);
  echo mysqli_num_rows($result);
 
+
 }
+
 
 
