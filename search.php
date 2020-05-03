490bb52fc81725724e34739d75edb79f9c941362
Moophie <michael.van.lierde@hotmail.com>
2020-05-03 20:34:24 +0200

    Basic autocomplete for classroom search

--- search.php
+++ search.php
@@ -24,8 +24,6 @@ if (!empty($_POST)) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="css/phpbuddy.css">
-    <script type="text/javascript" src="js/jquery.min.js"></script>
-    <script type="text/javascript" src="js/search.js"></script>
     <title>Search</title>
 </head>
 
