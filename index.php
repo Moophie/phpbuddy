0fdea24c1712beccd7727fb80d3212f74ef27d2b
serafimay <49272435+serafimay@users.noreply.github.com>
2020-05-10 12:27:22 +0200

    index css

--- index.php
+++ index.php
@@ -80,14 +80,6 @@ $user_buddy = classes\Buddy\User::findBuddy($user->getEmail());
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:400,700&display=swap">
     <link rel="stylesheet" href="fonts/font-awesome-5/css/fontawesome-all.min.css">
     <title>IMD Buddy</title>
-    <style>
-        .matches {
-            max-width: 250px;
-            margin: 5px 20px;
-            padding: 10px;
-            border: solid black 1px;
-        }
-    </style>
 </head>
 
 <body>
@@ -109,8 +101,8 @@ $user_buddy = classes\Buddy\User::findBuddy($user->getEmail());
                 </div>
             </div>
         </div>
-        <div class="jumbotron float-right" style="width:40%; height:230px;">
-            <h4>Buddy application statistics</h4>
+        <div class="jumbotron float-right" style="width:46%; height:230px;">
+            <h2>Buddy application statistics</h2>
             <br>
             <div>
                 <p><strong>Registered users:</strong> <?php echo $registered_count ?> <?php ?> </p>
@@ -120,7 +112,7 @@ $user_buddy = classes\Buddy\User::findBuddy($user->getEmail());
             </div>
         </div>
     </div>
-    <div class="container">
+    <div class="container" >
         <div class="jumbotron">
             <h2>Find your classroom here:</h2>
             <form action="classrooms.php" method="get">
@@ -130,14 +122,13 @@ $user_buddy = classes\Buddy\User::findBuddy($user->getEmail());
             </form>
         </div>
     </div>
-    <div class="container">
-        <div class="jumbotron">
-            <div class="center" style="height:450px;">
+    <div class="container" style="margin-top: 20px;">
+        <div class="center">
+            <div class="jumbotron">
                 <!-- If the user has no buddy yet, show suggestions -->
-
                 <?php if (empty($user->getBuddy_id())) : ?>
-
                     <h2>Potential Buddies</h2>
+            </div>
                     <!-- Loop over all the other users -->
                     <?php foreach ($potential_matches as $potential_match) :
 
@@ -146,35 +137,41 @@ $user_buddy = classes\Buddy\User::findBuddy($user->getEmail());
 
                         //If the function returns a match, print it out
                         if (!empty($match) && (empty($match->buddy_id) || $match->buddy_id == $user->getId())) : ?>
-                            <div class="matches float-left" style="display:block">
-                                <h4><?= $match->fullname ?></h4>
-                                <img src="./uploads/<?= htmlspecialchars($match->profile_img) ?>" width="100px;" height="100px;" />
-                                <br>
-                                <br>
-                                <h6>Things you have in common:</h6>
-
+                        <div class="matches float-left">
+                                <h3><?= $match->fullname ?></h3>
+                                <div class="img-div" >
+                                <img class="img-match" src="./uploads/<?= htmlspecialchars($match->profile_img) ?>"  />
+                                </div>
+                
                                 <!-- Check all attributes for common ones and then print them out -->
                                 <?php if ($match->location == $user->getLocation()) : ?>
-                                    <p><?= "Location: " . htmlspecialchars($match->location); ?><p>
-                                        <?php endif; ?>
-                                        <?php if ($match->games == $user->getGames()) : ?>
-                                            <p><?= "Video games: " . htmlspecialchars($match->games); ?></p>
-                                        <?php endif; ?>
-                                        <?php if ($match->music == $user->getMusic()) : ?>
-                                            <p><?= "Music: " . htmlspecialchars($match->music); ?></p>
-                                        <?php endif; ?>
-                                        <?php if ($match->films == $user->getFilms()) : ?>
-                                            <p><?= "Movies: " . htmlspecialchars($match->films); ?></p>
-                                        <?php endif; ?>
-                                        <?php if ($match->books == $user->getBooks()) : ?>
-                                            <p><?= "Books: " . htmlspecialchars($match->books); ?></p>
-                                        <?php endif; ?>
-                                        <?php if ($match->study_pref == $user->getStudy_pref()) : ?>
-                                            <p><?= "Same study preferences: " . htmlspecialchars($match->study_pref); ?></p>
-                                        <?php endif; ?>
-                                        <?php if ($match->hobby == $user->getHobby()) : ?>
-                                            <p><?= "Hobby: " . htmlspecialchars($match->hobby); ?></p>
-                                        <?php endif; ?>
+                                    <h6>Location</h6>
+                                    <h5><?= htmlspecialchars($match->location); ?></h5>
+                                <?php endif; ?>
+                                <?php if ($match->games == $user->getGames()) : ?>
+                                    <h6>Video Games</h6>
+                                    <h5><?= htmlspecialchars($match->games); ?></h5>
+                                <?php endif; ?>
+                                <?php if ($match->music == $user->getMusic()) : ?>
+                                    <h6>Music</h6>
+                                    <h5><?= htmlspecialchars($match->music); ?></h5>
+                                <?php endif; ?>
+                                <?php if ($match->films == $user->getFilms()) : ?>
+                                    <h6>Movies</h6>
+                                    <h5><?= htmlspecialchars($match->films); ?></h5>
+                                <?php endif; ?>
+                                <?php if ($match->books == $user->getBooks()) : ?>
+                                    <h6>Books</h6>
+                                    <h5><?=  htmlspecialchars($match->books); ?></h5>
+                                <?php endif; ?>
+                                <?php if ($match->study_pref == $user->getStudy_pref()) : ?>
+                                    <h6>Study Preference</h6>
+                                    <h5><?= htmlspecialchars($match->study_pref); ?></h5>
+                                <?php endif; ?>
+                                <?php if ($match->hobby == $user->getHobby()) : ?>
+                                    <h6>Hobby</h6>
+                                    <h5><?=  htmlspecialchars($match->hobby); ?></h5>
+                                <?php endif; ?>
 
                                         <?php if ($match->buddy_id == $user->getId()) : ?>
                                             <form action="" method="POST">
@@ -190,58 +187,77 @@ $user_buddy = classes\Buddy\User::findBuddy($user->getEmail());
                                                 <input type="text" name="buddy_id" value="<?= htmlspecialchars($match->id) ?>" hidden>
                                                 <input type="text" name="buddy_name" value="<?= htmlspecialchars($match->fullname) ?>" hidden>
                                                 <input type="text" name="buddy_email" value="<?= htmlspecialchars($match->email) ?>" hidden>
-                                                <input type="submit" name="getBuddy" value="Send buddy request!">
+                                                <input class="button-sbm" type="submit" name="getBuddy" value="Send buddy request!">
                                             </form>
                                         <?php endif; ?>
+                                        </div>
 
-                            </div>
                         <?php endif; ?>
+
                     <?php endforeach; ?>
-                <?php else : ?>
-                    <?php if ($user_buddy->buddy_id == $user->getId()) : ?>
-                        <h2 style="margin-top:-30px; padding-bottom: 20px">Your buddy</h2>
+                </div>
+            </div>
+
+    <?php else : ?>
+    <?php if ($user_buddy->buddy_id == $user->getId()) : ?>
+        
+        <div class="container" style="margin-top: 20px;">
+            <div class="center">
+                <div class="jumbotron">
+                        <h2>Your buddy</h2>
                     <?php else : ?>
-                        <h2 style="margin-top:-30px; padding-bottom: 20px">You have sent a request to:</h2>
+                        <h2>You have sent a request to:</h2>
                     <?php endif; ?>
-                    <div class="d-flex flex-row border rounded">
-                        <div class="p-0 w-25">
-                            <img src="./uploads/<?= htmlspecialchars($user_buddy->profile_img); ?>" class="img-thumbnail border-0" />
-                        </div>
-                        <div class="pl-3 pt-2 pr-2 pb-2 w-75 border-left">
-                            <h4 class="text-primary"><?= htmlspecialchars($user_buddy->fullname); ?></h4>
-                            <h6>Things you have in common:</h6>
+                </div>
+
+                    <div class="matches float-left">
+                        <h3><?= htmlspecialchars($user_buddy->fullname); ?></h3>
+                        <img class="img-match" src="./uploads/<?= htmlspecialchars($user_buddy->profile_img); ?>" />
+
                             <!-- Check all attributes for common ones and then print them out -->
-                            <?php if ($user_buddy->location == $user->getLocation()) : ?>
-                                <p><?= "Location: " . htmlspecialchars($user_buddy->location); ?><p>
+                                    <?php if ($user_buddy->location == $user->getLocation()) : ?>
+                                        <h6>Location</h6>
+                                        <h5><?= htmlspecialchars($user_buddy->location); ?><h5>
                                     <?php endif; ?>
+
                                     <?php if ($user_buddy->games == $user->getGames()) : ?>
-                                        <p><?= "Video games: " . htmlspecialchars($user_buddy->games); ?></p>
+                                        <h6>Video Games</h6>
+                                        <h5><?= htmlspecialchars($user_buddy->games); ?></h5>
                                     <?php endif; ?>
+
                                     <?php if ($user_buddy->music == $user->getMusic()) : ?>
-                                        <p><?= "Music: " . htmlspecialchars($user_buddy->music); ?></p>
+                                        <h6>Music</h6>
+                                        <h5><?= htmlspecialchars($user_buddy->music); ?></h5>
                                     <?php endif; ?>
+
                                     <?php if ($user_buddy->films == $user->getFilms()) : ?>
-                                        <p><?= "Movies: " . htmlspecialchars($user_buddy->films); ?></p>
+                                        <h6>Movies</h6>
+                                        <h5><?= htmlspecialchars($user_buddy->films); ?></h5>
                                     <?php endif; ?>
+
                                     <?php if ($user_buddy->books == $user->getBooks()) : ?>
-                                        <p><?= "Books: " . htmlspecialchars($user_buddy->books); ?></p>
+                                        <h6>Books</h6>
+                                        <h5><?= htmlspecialchars($user_buddy->books); ?></h5>
                                     <?php endif; ?>
+
                                     <?php if ($user_buddy->study_pref == $user->getStudy_pref()) : ?>
-                                        <p><?= "Same study preferences: " . htmlspecialchars($user_buddy->study_pref); ?></p>
+                                        <h6>Study Preference</h6>
+                                        <h5><?= htmlspecialchars($user_buddy->study_pref); ?></h5>
                                     <?php endif; ?>
+
                                     <?php if ($user_buddy->hobby == $user->getHobby()) : ?>
-                                        <p><?= "Hobby: " . htmlspecialchars($user_buddy->hobby); ?></p>
+                                        <h6>Hobby</h6>
+                                        <h5><?= htmlspecialchars($user_buddy->hobby); ?></h5>
                                     <?php endif; ?>
+
                                     <form action="" method="POST">
                                         <input type="text" name="buddy_id" value="<?php echo $user_buddy->buddy_id; ?>" hidden>
                                         <input type="submit" class="btn btn-danger" name="unmatch" value="Unmatch Buddy">
                                     </form>
-                        </div>
-                    </div>
+                            </div>
                 <?php endif; ?>
             </div>
         </div>
-    </div>
 
 
 
